556ced0739eb1649f83cc754e3b27d67d55c3c78
nicolaszamarreno <zamarreno.nicolas@gmail.com>
2018-11-04 23:04:44 +0100

    Change name of project

---
+++ boilerplate.Tests/Controllers/UserControllerTest.cs
@@ -0,0 +1,55 @@
+﻿namespace Boilerplate.Tests.Controllers
+{
+    using Boilerplate.Controllers;
+    using Boilerplate.Entities;
+    using Boilerplate.Repositories;
+    using Boilerplate.Repositories.Contrat;
+    using Boilerplate.Tests.Helpers;
+    using Boilerplate.ViewModel;
+    using Microsoft.AspNetCore.Mvc;
+    using Moq;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class UserControllerTest
+    {
+        private readonly DatabaseContext _context;
+        public UserControllerTest()
+        {
+            _context = GetContext.ReturnContext();
+        }
+
+        //[Fact]
+        //public async Task UserController_Login_ReturnsNotFound()
+        //{
+        //    // Arrange 
+        //    User userToAdd = new UserHelper().CreateUser();
+        //    Mock<IUserRepository> _userRepository = new Mock<IUserRepository>();
+
+        //    // Act
+        //    LoginViewModel loginPass = new LoginViewModel { Email = userToAdd.Email, Password = userToAdd.Password };
+        //    var _controller = new UserController(_userRepository.Object);
+        //    var result = await _controller.Login(loginPass);
+
+        //    // Assert
+        //    Assert.IsType<NotFoundResult>(result);
+        //}
+
+        //[Fact]
+        //public async void UserController_Login_ReturnsOk()
+        //{
+        //    // Arrange 
+        //    User userToAdd = new UserHelper().CreateUser();
+        //    Mock<IUserRepository> _userRepository = new Mock<IUserRepository>();
+        //    _userRepository.Setup(x => x.GetUserByLogin(userToAdd.Email, userToAdd.Password)).ReturnsAsync(userToAdd);
+
+        //    // Act
+        //    LoginViewModel loginPass = new LoginViewModel { Email = userToAdd.Email, Password = userToAdd.Password };
+        //    var _controller = new UserController(_userRepository.Object);
+        //    var result = await _controller.Login(loginPass);
+
+        //    // Assert
+        //    //Assert.IsType<OkObjectResult>(result.ExecuteResult);
+        //}
+    }
+}
---
+++ boilerplate/Controllers/UserController.cs
@@ -0,0 +1,106 @@
+﻿namespace Boilerplate.Controllers
+{
+    using Microsoft.AspNetCore.Authentication;
+    using Microsoft.AspNetCore.Authentication.Cookies;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Collections.Generic;
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+    using Boilerplate.Repositories.Contrat;
+    using Boilerplate.Entities;
+    using Boilerplate.ViewModel;
+
+    [Route("api/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+        //private readonly UserManager<User> _userManager;
+
+        public UserController(IUserRepository userRepository)
+        {
+            // UserManager<User> userManager ==> inject
+            _userRepository = userRepository;
+            //_userManager = userManager;
+        }
+
+        // Warning : Send data with header Content-Type: application/json
+        [HttpPost]
+        public async Task<ActionResult> Login([FromBody] LoginViewModel model)
+        {
+            User userIsAuthenticated = await _userRepository.GetUserByLogin(model.Email, model.Password);
+
+            if (userIsAuthenticated == null)
+            {
+                return NotFound();
+            }
+
+
+            List<Claim> claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.Name, userIsAuthenticated.LastName));
+            claims.Add(new Claim(ClaimTypes.Role, userIsAuthenticated.Role));
+            claims.Add(new Claim(ClaimTypes.Email, userIsAuthenticated.Email));
+
+            var userIdentity = new ClaimsIdentity(claims);
+
+            ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
+            await HttpContext.SignInAsync(principal);
+
+            return Ok();
+        }
+
+        [HttpPost("~/api/user/logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return Ok();
+        }
+
+        //private Task<User> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult<List<User>>> GetAll()
+        {
+            //User usr = await GetCurrentUserAsync();
+
+            List<User> users = await _userRepository.GetAll();
+
+            if (users == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(users);
+        }
+
+        [HttpPost("~/api/user/register")]
+        public async Task<ActionResult<User>> Register([FromBody] RegisterViewModel user)
+        {
+            User userToRegister = new User
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                Password = user.Password,
+                Role = "Admin"
+            };
+
+            await _userRepository.Add(userToRegister);
+
+            return Ok(userToRegister);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
